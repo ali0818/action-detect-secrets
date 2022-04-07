05de67a7114cf4536626018b517cc6dcf2d667b2
human <human@humans-MacBook-Pro.local>
2022-04-07 14:32:38 +0530

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,5 @@ detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} ${INPUT_WORKDIR} \
         -reporter="${INPUT_REPORTER:-github-pr-check}" \
         -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
         -level="${INPUT_LEVEL}" \
-        ${INPUT_REVIEWDOG_FLAGS}
+        ${INPUT_REVIEWDOG_FLAGS} \
     | slack -slack_token="${INPUT_SLACK_TOKEN}"   
